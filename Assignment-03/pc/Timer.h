2ad29b5443a622938eaffb1e695c9dabed688751
Bobo-bug-lab <645498374@qq.com>
2023-03-14 16:32:47 +0100

    added sth

--- Assignment-03/pc/Timer.h
+++ Assignment-03/pc/Timer.h
@@ -7,12 +7,16 @@
 class Timer {
 private:
     bool timerFlag;
+    std::chrono::high_resolution_clock::time_point m_start_time;
 public:
     Timer();
     void setupFreq(int freq);
     void setupPeriod(int period);
     void waitForNextTick();
     bool check_time_since(long time_from, long period);
+    std::string getCurrentTime();
+    int getCurrentHour();
 };
 
 #endif
+
