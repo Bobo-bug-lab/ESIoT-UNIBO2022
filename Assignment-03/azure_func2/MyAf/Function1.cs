34d4b16b7336687e4cbd46dec8d1ead49cf25175
Bobo-bug-lab <645498374@qq.com>
2023-04-07 22:24:56 +0200

    azure developing routing0407

--- Assignment-03/azure_func2/MyAf/Function1.cs
+++ Assignment-03/azure_func2/MyAf/Function1.cs
@@ -1,25 +1,68 @@
 using IoTHubTrigger = Microsoft.Azure.WebJobs.EventHubTriggerAttribute;
 
+using System;
+using Microsoft.Azure.Devices;
+using Newtonsoft.Json;
+using System.Text;
+using System.Configuration;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.EventHubs;
-using System.Text;
 using System.Net.Http;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Rest;
 
 namespace MyAf
 {
     public class Function1
     {
+        private static readonly ServiceClient _serviceClient = ServiceClient.CreateFromConnectionString("HostName=bobofan-iothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=QcR4qbxQNdytJGwKCyib2MNeF+77HFv7I6GkbNCKfYc=");
+        private string sourceDeviceId;
+        private const string pcString = "HostName=bobofan-iothub.azure-devices.net;DeviceId=pc;SharedAccessKey=7erHmTK/12my6evlrkn8YJiHF3hDlrr6TkTFHN6EQh0=";
+        private const string espString = "HostName=bobofan-iothub.azure-devices.net;DeviceId=esp8266;SharedAccessKey=Peu+kM0TUssgYbd3Wra7vt9T0SBfZEeomQnLp8Z+6fw=";
+
+        //private const string rednodeString;
+
         private static HttpClient client = new HttpClient();
         
         [FunctionName("Function1")]
-        public void Run([IoTHubTrigger("messages/events", Connection = "ConnectionString")] EventData[] eventHubMessages, ILogger log)
+        public async Task RunAsync([IoTHubTrigger("messages/events", Connection = "ConnectionString")] EventData message, ILogger log)
         {
-            foreach (var message in eventHubMessages)
+            
+
+            var sourceDeviceId = message.SystemProperties["iothub-connection-device-id"].ToString();
+            var messageString = Encoding.UTF8.GetString(message.Body.Array);
+            log.LogInformation($"C# function triggered to process a message: {messageString}");
+            log.LogInformation($"Device ID: {sourceDeviceId}");
+
+
+            //switch (this.sourceDeviceId)
+            //{
+            //    case "esp":
+            //        Console.WriteLine("Monday");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Sunday");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Nothing");
+            //}
+
+            try
             {
-                log.LogInformation($"C# function triggered to process a message: {Encoding.UTF8.GetString(message.Body)}");
-                log.LogInformation($"EnqueuedTimeUtc={message.SystemProperties.EnqueuedTimeUtc}");
+                string targetDeviceId = "pc";
+                var c2dMessage = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                // Send the message to the device with the corresponding device ID
+                await _serviceClient.SendAsync(targetDeviceId, c2dMessage);
+
+                log.LogInformation($"C2D message sent to device with ID: {targetDeviceId}");
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"Exception caught: {ex.Message}");
             }
         }
     }
---
+++ Assignment-03/azure_func2/MyAf/Message.cs
@@ -0,0 +1,5 @@
+﻿public class DeviceMessage
+{
+    public string DeviceId { get; set; }
+    public string Message { get; set; }
+}
